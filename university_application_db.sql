7980fd96b4c1a13f0f2003f15aa682f0378e1c24
Franc Bruzja <francbruzja@gmail.com>
2023-02-04 13:21:37 +0100

    fix: db

--- university_application_db.sql
+++ university_application_db.sql
@@ -12,10 +12,13 @@ user_id integer primary key not null,
 first_name varchar(20) not null,
 last_name varchar(20) not null,
 email varchar(30) not null,
-password text not null
+password text not null,
 major varchar(30) not null,
 minor varchar(30) not null,
-courses text[]
+courses text[],
+profile_picture varchar(10000),
+notifications boolean,
+friends integer[]
 );
 
 create table ua_categories(
@@ -24,12 +27,30 @@ user_id integer not null,
 title varchar(20) not null,
 description varchar(50) not null,
 course_time varchar(50) not null,
-location varchar(50) not null
-attendees text[] not null
+location varchar(50) not null,
+attendees text[] not null,
 );
 
-alter table ua_categories add constraint cat_users_fk
-foreign key (user_id) references ua_users(user_id);
+CREATE TABLE IF NOT EXISTS ua_comments
+(
+    id integer primary key not null,
+    content character varying(6000),
+    author character varying(6000),
+    likes integer,
+    course integer,
+    likedby integer[]
+)
+
+CREATE TABLE IF NOT EXISTS friendship
+(
+    friendship_id integer primary key NOT NULL,
+    user_id integer,
+    friend_id integer,
+    status character varying(100),
+    notified boolean
+);
 
 create sequence ua_users_seq increment 1 start 1;
 create sequence ua_categories_seq increment 1 start 1;
+create sequence ua_comments_seq increment 1 start 1;
+create sequence friendship_seq increment 1 start 1;
